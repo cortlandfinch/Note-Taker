68eb34368df2d88267e0c8d2c8e7d3c02dba7d20
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-08-10 19:00:27 -0400

    add start script to package.json file

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,22 +1,33 @@
 
 const fs = require("fs");
-const userNotes = require('../../db/db.json');
 const router = require('express').Router();
-const uuid = require('uuid');
+const userNotes = require('../../db/db.json');
+// universal unique identifier "uuid" npm package to set unique id to note
+const { v4: uuidv4} = require('uuid');
 
 // GET and POST routes for APIs for notes to save and go in db.json file
 // Using GET to retrieve notes to db
-router.get('/api/notes', (req, res) => {
+// Takes the notes user creates to the notes page
+router.get('/notes', (req, res) => {
     res.json(userNotes)
 });
 
 // Using POST to add the notes to the db
-router.post("/api/notes", (req, res) => {
-    userNotes.push(req.body);
+router.post("/notes", (req, res) => {
+    // using newNoteId for uuid so each note gets its own unique id everytime it is called
+    const newNoteId = uuidv4();
+    // usn
+    const userNote = {
+        id: newNoteId,
+        title: req.body.title,
+        text: req.body.text
+    };
+    userNotes.push(userNote);
+    res.json(userNote);
     fs.writeFileSync('./db/db.json', JSON.stringify(userNotes));
-    res.json(req.body);
 });
 
 // Using DELETE
+router.delete("/notes/:id")
 
 module.exports = router;
